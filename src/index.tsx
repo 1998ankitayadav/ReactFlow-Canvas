583ac9ffb47448c9c756d10e89f3e1c32a14389e
Ankita Yadav <ay8964925@gmail.com>
2026-06-15 23:02:33 +0530

    Fix entry point reference in index.html to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@xyflow/react/dist/style.css";
 import App from "./App";
 import "./styles.css";
 
-const rootElement = document.getElementById("root")!;
-const root = ReactDOM.createRoot(rootElement);
+const rootElement = document.getElementById("root");
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Check index.html for <div id='root'></div>");
+}
+
